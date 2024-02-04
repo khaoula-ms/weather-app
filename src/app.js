a9e060ad5beb8657101d4c1f5181ffe32973116c
khaoula-ms <158272529+khaoula-ms@users.noreply.github.com>
2024-02-04 18:29:10 +0100

    injected more weather data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,43 @@
 function updateWeather(response) {
   let temperature = document.querySelector("#temperature-value");
   let city = document.querySelector("#weather-app-city");
+  let weatherDescription = document.querySelector("#weather-description");
+  let country = document.querySelector("#country");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let feelsLikeTemp = document.querySelector("#feels-like");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   city.innerHTML = response.data.city;
+
+  timeElement.innerHTML = formatDate(date);
   temperature.innerHTML = Math.round(response.data.temperature.current);
+  weatherDescription.innerHTML = response.data.condition.description;
+  country.innerHTML = response.data.country;
+  humidity.innerHTML = response.data.temperature.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  feelsLikeTemp.innerHTML = Math.round(response.data.temperature.feels_like);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 function searchWeatherCity(city) {
   let apiKey = "e3b5tabo33a51804d1f4de7a47bd9d3f";
